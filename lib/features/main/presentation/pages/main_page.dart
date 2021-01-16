652023224a0474c80eeb4f546b9c48047698781a
3d24rd0 <eduardo.martinez.catala@gmail.com>
2021-01-16 08:08:18 +0100

    try show message

--- lib/features/main/presentation/pages/main_page.dart
+++ lib/features/main/presentation/pages/main_page.dart
@@ -20,58 +20,88 @@ class MainPage extends StatelessWidget {
         child: Scaffold(
           backgroundColor: Theme.of(context).backgroundColor,
           body: SafeArea(
-              child: ExcludeSemantics(
-            excluding: true,
-            child: BlocProvider<MainBloc>(
-              create: (_) => getIt<MainBloc>()..add(LoadEvent()),
-              child: BlocBuilder<MainBloc, MainState>(
-                buildWhen: (previous, current) =>
-                    previous?.runtimeType != current?.runtimeType,
-                builder: (context, state) {
-                  return Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Row(
-                        children: [
-                          SizedBox(
-                            // height: DinamicSize.heightSize(context, 550),
-                            width: DinamicSize.heightSize(context, 550),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: ListProducts(),
+            child: ExcludeSemantics(
+              excluding: true,
+              child: BlocProvider<MainBloc>(
+                create: (_) => getIt<MainBloc>()..add(LoadEvent()),
+                child: BlocListener<MainBloc, MainState>(
+                  listenWhen: (previous, current) =>
+                      previous.clientId != current.clientId &&
+                      (current?.clientId?.isNotEmpty ?? false),
+                  listener: (context, state) {
+                    var message = state?.clientId ?? "";
+                    if (message.isEmpty) return;
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                        //onVisible: () => Future.delayed(Duration(seconds: 3)),
+                        backgroundColor: Color(0xff18469f),
+                        content: SizedBox(
+                          height: DinamicSize.heightSize(context, 50),
+                          child: Center(
+                            child: Container(
+                              child: Text(
+                                message,
+                                style: TextStyle(
+                                  color: Color(0xfffaf900),
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400,
+                                  fontStyle: FontStyle.normal,
+                                  letterSpacing: 0.24,
+                                ),
+                              ),
                             ),
                           ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                color: Colors.white,
+                        )));
+                  },
+                  child: BlocBuilder<MainBloc, MainState>(
+                    buildWhen: (previous, current) =>
+                        previous?.runtimeType != current?.runtimeType,
+                    builder: (context, state) {
+                      return Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                // height: DinamicSize.heightSize(context, 550),
+                                width: DinamicSize.heightSize(context, 550),
                                 child: Padding(
-                                  padding: const EdgeInsets.all(20.0),
-                                  child: ProductView(),
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: ListProducts(),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Container(
+                                    color: Colors.white,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(20.0),
+                                      child: ProductView(),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          Visibility(
+                            visible: state?.runtimeType != InitializedState,
+                            child: Positioned.fill(
+                              child: Container(
+                                color: Colors.blue,
+                                child: Center(
+                                  child: CircularIndicator(),
                                 ),
                               ),
                             ),
                           )
                         ],
-                      ),
-                      Visibility(
-                        visible: state?.runtimeType != InitializedState,
-                        child: Positioned.fill(
-                          child: Container(
-                            color: Colors.blue,
-                            child: Center(
-                              child: CircularIndicator(),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  );
-                },
+                      );
+                    },
+                  ),
+                ),
               ),
             ),
-          )),
+          ),
         ),
       ),
     );
