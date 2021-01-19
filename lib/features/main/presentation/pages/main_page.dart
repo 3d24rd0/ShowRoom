cb29be514502fb775806ab8e9c7cfdaae54484c7
3d24rd0 <eduardo.martinez.catala@gmail.com>
2021-01-19 13:05:03 +0100

    add header

--- lib/features/main/presentation/pages/main_page.dart
+++ lib/features/main/presentation/pages/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:showroom/core/tools/dinamic_size.dart';
 import 'package:showroom/features/main/presentation/bloc/main_bloc.dart';
+import 'package:showroom/features/main/presentation/widgets/body.dart';
 import 'package:showroom/features/main/presentation/widgets/circular_indicator.dart';
 import 'package:showroom/features/main/presentation/widgets/list_products.dart';
 import 'package:showroom/features/main/presentation/widgets/product_view.dart';
@@ -59,30 +60,7 @@ class MainPage extends StatelessWidget {
                       return Stack(
                         alignment: Alignment.center,
                         children: [
-                          Row(
-                            children: [
-                              SizedBox(
-                                // height: DinamicSize.heightSize(context, 550),
-                                width: DinamicSize.heightSize(context, 550),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ListProducts(),
-                                ),
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    color: Colors.white,
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(20.0),
-                                      child: ProductView(),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
+                          Body(),
                           Visibility(
                             visible: state?.runtimeType != InitializedState,
                             child: Positioned.fill(
---
+++ lib/features/main/presentation/widgets/body.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:showroom/core/tools/dinamic_size.dart';
+import 'package:showroom/features/main/presentation/bloc/main_bloc.dart';
+
+import 'list_products.dart';
+import 'product_view.dart';
+
+class Body extends StatelessWidget {
+  const Body({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _Header(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xffCCCCCC),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            color: Color(0xffC2C2C2),
+            height: DinamicSize.heightSize(context, 181),
+            width: DinamicSize.heightSize(context, 389),
+            child: Center(
+              child: Text(
+                "Collections".toUpperCase(),
+                style: TextStyle(
+                  color: Color(0xFFFFFFFF),
+                  fontSize: 22,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.italic,
+                  letterSpacing: 0.24,
+                ),
+              ),
+            ),
+          ),
+          Container(
+              padding: const EdgeInsets.only(right: 80),
+              height: DinamicSize.heightSize(context, 181),
+              width: DinamicSize.widthSize(context, 1400),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  BlocBuilder<MainBloc, MainState>(
+                      buildWhen: (previous, current) =>
+                          previous?.selectedProduct != current?.selectedProduct,
+                      builder: (context, state) {
+                        return Text(
+                          (state?.selectedProduct?.name ?? "").toUpperCase(),
+                          overflow: TextOverflow.fade,
+                          maxLines: 1,
+                          style: TextStyle(
+                            color: Color(0xFFFFFFFF),
+                            fontSize: 77,
+                            fontWeight: FontWeight.w600,
+                            fontStyle: FontStyle.normal,
+                            letterSpacing: 0.24,
+                          ),
+                        );
+                      }),
+                  Text(
+                    "colección ‧ collection".toUpperCase(),
+                    style: TextStyle(
+                      color: Color(0xFF000000),
+                      fontSize: 17,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                      letterSpacing: 0.24,
+                    ),
+                  ),
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
