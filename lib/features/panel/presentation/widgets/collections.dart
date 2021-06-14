92dd967f4e7b7268488506eb2c34638eab7682a5
3d24rd0 <eduardo.martinez.catala@gmail.com>
2021-06-14 02:16:33 +0200

    fix text index

--- lib/features/panel/presentation/widgets/collections.dart
+++ lib/features/panel/presentation/widgets/collections.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:showroom/core/tools/dinamic_size.dart';
+import 'package:showroom/features/panel/domain/entities/collection.dart';
 import 'package:showroom/features/panel/presentation/bloc/panel_bloc.dart';
 
 import '../../../../core/widgets/collection_title.dart';
+import 'package:showroom/features/products/domain/entities/variant.dart';
+import 'package:showroom/features/products/domain/entities/product.dart';
 
 class Collections extends StatelessWidget {
   final bool left;
@@ -36,98 +39,102 @@ class Collections extends StatelessWidget {
                 final selectedVariant = state.selectedVariant;
 
                 return Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 30.h(context)),
-                    child: Scrollbar(
-                      child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: collection.length,
-                          itemBuilder: (BuildContext ctx, index) {
-                            return InkWell(
-                              onTap: () => BlocProvider.of<PanelBloc>(context)
-                                  .add(SelectEvent(collection[index])),
-                              child: Row(
-                                mainAxisAlignment: left
-                                    ? MainAxisAlignment.start
-                                    : MainAxisAlignment.end,
-                                children: [
-                                  Visibility(
-                                    visible: left &&
-                                        collection[index].productId ==
-                                            selectedProduct?.id &&
-                                        collection[index]
-                                                .variantId
-                                                ?.toLowerCase() ==
-                                            selectedVariant?.id
-                                                ?.toLowerCase(),
-                                    child: Icon(
-                                      Icons.arrow_left,
-                                      color: Color(0xff6CF149),
-                                      size: 50.w(context),
-                                    ),
-                                    replacement: Container(
-                                      width: 50.w(context),
-                                      height: 50.w(context),
-                                    ),
+                  child: Scrollbar(
+                    child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: collection.length,
+                        itemBuilder: (BuildContext ctx, index) {
+                          return InkWell(
+                            onTap: () => BlocProvider.of<PanelBloc>(context)
+                                .add(SelectEvent(collection[index])),
+                            child: Row(
+                              mainAxisAlignment: left
+                                  ? MainAxisAlignment.start
+                                  : MainAxisAlignment.end,
+                              children: [
+                                Visibility(
+                                  visible: left &&
+                                      collection[index].productId ==
+                                          selectedProduct?.id &&
+                                      collection[index]
+                                              .variantId
+                                              ?.toLowerCase() ==
+                                          selectedVariant?.id?.toLowerCase(),
+                                  child: Icon(
+                                    Icons.arrow_left,
+                                    color: Color(0xff6CF149),
+                                    size: 50.w(context),
                                   ),
-                                  Flexible(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text(
-                                        getProductTitle(
-                                          collection[index].name ?? "",
-                                          left,
-                                          (index + 1).toString(),
+                                  replacement: Container(
+                                    width: 50.w(context),
+                                    height: 50.w(context),
+                                  ),
+                                ),
+                                Flexible(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      mainAxisAlignment: left
+                                          ? MainAxisAlignment.start
+                                          : MainAxisAlignment.end,
+                                      children: [
+                                        Visibility(
+                                          visible: left,
+                                          child: _text(
+                                              (index + 1).toString() + " ",
+                                              collection,
+                                              index,
+                                              selectedProduct,
+                                              selectedVariant,
+                                              context),
                                         ),
-                                        maxLines: collection[index].maxLines,
-                                        overflow: TextOverflow.ellipsis,
-                                        textAlign: left
-                                            ? TextAlign.left
-                                            : TextAlign.right,
-                                        style: TextStyle(
-                                          color: collection[index].productId ==
-                                                      selectedProduct?.id &&
-                                                  collection[index]
-                                                          .variantId
-                                                          ?.toUpperCase() ==
-                                                      selectedVariant?.id
-                                                          ?.toUpperCase()
-                                              ? Color(0xff6CF149)
-                                              : Color(0xffA0A1A2),
-                                          fontSize:
-                                              DinamicSize.fontSize(context, 20),
-                                          fontWeight: FontWeight.bold,
-                                          letterSpacing: 0.2,
-                                          height: 1.3,
-                                          fontFamily: 'Montserrat',
+                                        Flexible(
+                                          child: _text(
+                                              getProductTitle(
+                                                collection[index].name ?? "",
+                                              ),
+                                              collection,
+                                              index,
+                                              selectedProduct,
+                                              selectedVariant,
+                                              context),
                                         ),
-                                      ),
+                                        Visibility(
+                                          visible: !left,
+                                          child: _text(
+                                              " " + (index + 1).toString(),
+                                              collection,
+                                              index,
+                                              selectedProduct,
+                                              selectedVariant,
+                                              context),
+                                        ),
+                                      ],
                                     ),
                                   ),
-                                  Visibility(
-                                    visible: !left &&
-                                        collection[index].productId ==
-                                            selectedProduct?.id &&
-                                        collection[index]
-                                                .variantId
-                                                ?.toUpperCase() ==
-                                            selectedVariant?.id
-                                                ?.toUpperCase(),
-                                    child: Icon(
-                                      Icons.arrow_right,
-                                      size: 50.w(context),
-                                      color: Color(0xff6CF149),
-                                    ),
-                                    replacement: Container(
-                                      width: 50.w(context),
-                                      height: 50.w(context),
-                                    ),
+                                ),
+                                Visibility(
+                                  visible: !left &&
+                                      collection[index].productId ==
+                                          selectedProduct?.id &&
+                                      collection[index]
+                                              .variantId
+                                              ?.toUpperCase() ==
+                                          selectedVariant?.id?.toUpperCase(),
+                                  child: Icon(
+                                    Icons.arrow_right,
+                                    size: 50.w(context),
+                                    color: Color(0xff6CF149),
                                   ),
-                                ],
-                              ),
-                            );
-                          }),
-                    ),
+                                  replacement: Container(
+                                    width: 50.w(context),
+                                    height: 50.w(context),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }),
                   ),
                 );
               }),
@@ -136,13 +143,35 @@ class Collections extends StatelessWidget {
     );
   }
 
-  String getProductTitle(String name, bool left, String index) {
-    String title = name[0].toUpperCase() + name.substring(1).toLowerCase();
-    if (left) {
-      title = index + " " + title;
-    } else {
-      title = title + " " + index;
-    }
-    return title;
+  Text _text(
+    String text,
+    List<Collection> collection,
+    int index,
+    Product? selectedProduct,
+    Variant? selectedVariant,
+    BuildContext context,
+  ) {
+    return Text(
+      text,
+      maxLines: collection[index].maxLines,
+      overflow: TextOverflow.ellipsis,
+      textAlign: left ? TextAlign.left : TextAlign.right,
+      style: TextStyle(
+        color: collection[index].productId == selectedProduct?.id &&
+                collection[index].variantId?.toUpperCase() ==
+                    selectedVariant?.id?.toUpperCase()
+            ? Color(0xff6CF149)
+            : Color(0xffA0A1A2),
+        fontSize: DinamicSize.fontSize(context, 20),
+        fontWeight: FontWeight.bold,
+        letterSpacing: 0.2,
+        height: 1.3,
+        fontFamily: 'Montserrat',
+      ),
+    );
+  }
+
+  String getProductTitle(String name) {
+    return name[0].toUpperCase() + name.substring(1).toLowerCase();
   }
 }
